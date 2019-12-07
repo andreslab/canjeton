039249c443702dfd85bfcfa8c2abf09d44e3ba2d
Jaime Andrade <jaimeandrade454@gmail.com>
2019-12-06 22:17:45 -0500

    add request coupons

--- lib/api/coupon.dart
+++ lib/api/coupon.dart
@@ -10,8 +10,131 @@ import '../utils/session.dart';
 class CouponAPI {
 
   final _session = Session();
-  //obtiene una respuesya futura
-  Future<bool> coupons(BuildContext context) async {
+
+  Future<bool> getCouponsList(BuildContext context) async {
+
+    try {
+
+      var uri = Uri.https(AppConfig.apiHost, "/api/v1/coupons");
+
+      final http.Response response = await http.get(uri);
+    
+      //final responseString = response.body;
+
+      print("GET COUPON LIST | URL: " + uri.toString());
+
+      final parsed = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        final token = parsed["token"] as String;
+        final expiresIn = parsed["expiresIn"] as int;
+        print("response 200: ${response.body}");
+        
+        //save token
+        await _session.set(token, expiresIn);
+
+        return true;
+      }else if (response.statusCode == 500){
+        //lanza excepcion
+        throw PlatformException(code: "500", message: parsed["message"]);
+      }
+
+      throw PlatformException(code: "201", message: "Error getCouponsList");
+
+    } on PlatformException catch(e){
+      print("Error ${e.code}:${e.message}");
+      Dialogs.alert(context, title: "ERROR", message: e.message, onOk: (){
+
+      });
+      return false;
+    }
+  }
+
+  Future<bool> getCouponById(BuildContext context,
+  {@required String id}) async {
+
+    try {
+
+      var uri = Uri.https(AppConfig.apiHost, "/api/v1/coupons/{$id}");
+
+      final http.Response response = await http.get(uri);
+    
+      //final responseString = response.body;
+
+      print("GET COUPON BY ID | URL: " + uri.toString());
+
+      final parsed = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        final token = parsed["token"] as String;
+        final expiresIn = parsed["expiresIn"] as int;
+        print("response 200: ${response.body}");
+        
+        //save token
+        await _session.set(token, expiresIn);
+
+        return true;
+      }else if (response.statusCode == 500){
+        //lanza excepcion
+        throw PlatformException(code: "500", message: parsed["message"]);
+      }
+
+      throw PlatformException(code: "201", message: "Error getCouponById");
+
+    } on PlatformException catch(e){
+      print("Error ${e.code}:${e.message}");
+      Dialogs.alert(context, title: "ERROR", message: e.message, onOk: (){
+
+      });
+      return false;
+    }
+  }
+
+  Future<bool> saveCouponByUser(BuildContext context, 
+  {@required String idUser, 
+  @required String idCoupon}) async {
+    try {
+      final url = "${AppConfig.apiHost}/api/auth/coupon";
+
+      final http.Response response = await http.post(url,
+      body: {
+        "id_user": idUser,
+        "id_coupon": idCoupon,});
+    
+      //final responseString = response.body;
+
+      final parsed = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        final token = parsed["token"] as String;
+        final expiresIn = parsed["expiresIn"] as int;
+        print("response 200: ${response.body}");
+        
+        //save token
+        await _session.set(token, expiresIn);
+
+        return true;
+      }else if (response.statusCode == 500){
+        //lanza excepcion
+        throw PlatformException(code: "500", message: parsed["message"]);
+      }
+
+      throw PlatformException(code: "201", message: "Error saveCouponByUser");
+
+    } on PlatformException catch(e){
+      print("Error ${e.code}:${e.message}");
+      Dialogs.alert(context, title: "ERROR", message: e.message, onOk: (){
+        
+      });
+      return false;
+    }
+  }
+}
+
+
+/*
+//obtiene una respuesya futura
+  Future<bool> noName(BuildContext context) async {
 
     Map<String, String> params = {
         "title": "Cerveza 20perla 20del 20pacifico 20cupon 201", 
@@ -60,47 +183,4 @@ class CouponAPI {
       return false;
     }
   }
-
-  Future<bool> login(BuildContext context, 
-  {@required String email, 
-  @required String password}) async {
-    try {
-      final url = "${AppConfig.apiHost}/oauth/token";
-
-      final http.Response response = await http.post(url,
-      body: {
-        "client_id": "2",
-        "client_secret": "In6mJSpHJBOXQg0VH9f561uDsAKEVCOj2Eg2IzO0",
-        "username": email,  
-        "password": password,
-        "scope":""});
-    
-      //final responseString = response.body;
-
-      final parsed = jsonDecode(response.body);
-
-      if (response.statusCode == 200) {
-        final token = parsed["token"] as String;
-        final expiresIn = parsed["expiresIn"] as int;
-        print("response 200: ${response.body}");
-        
-        //save token
-        await _session.set(token, expiresIn);
-
-        return true;
-      }else if (response.statusCode == 500){
-        //lanza excepcion
-        throw PlatformException(code: "500", message: parsed["message"]);
-      }
-
-      throw PlatformException(code: "201", message: "Error /register");
-
-    } on PlatformException catch(e){
-      print("Error ${e.code}:${e.message}");
-      Dialogs.alert(context, title: "ERROR", message: e.message, onOk: (){
-        
-      });
-      return false;
-    }
-  }
-}
+*/
